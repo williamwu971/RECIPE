fed93c44b8ecc89580189548ce7238b5a7433c59
William <xiwu9085@uni.sydney.edu.au>
2022-04-21 17:26:16 +1000

    -AUTO- Thu Apr 21 17:26:16 AEST 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -36,9 +36,9 @@ void run(char **argv) {
                  * the size of value should change depending on the key
                  */
 
-                int size = rand()%2048;
-                void* value = malloc(size);
-
+                int size = rand()%2048+sizeof(uint64_t);
+                uint64_t * value = (uint64_t *)malloc(size);
+                *value=keys[i];
 
 
                 tree->put(keys[i], value, t);
