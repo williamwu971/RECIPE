a44ba1ac7b37ddb7030c5dd6e1a7327027c241fd
William <xiwu9085@uni.sydney.edu.au>
2022-04-22 15:07:19 +1000

    -AUTO- Fri Apr 22 15:07:19 AEST 2022

--- P-Masstree/run.bash
+++ P-Masstree/run.bash
@@ -2,12 +2,23 @@
 
 for var in "$@"; do
   if [ "$var" = "build" ]; then
+
+    # build ralloc
+    cd /home/xiaoxiang/ralloc/test/
+    git pull
+    make clean
+    make libralloc.a
+
+    # build P-Masstree
+    cd /home/xiaoxiang/RECIPE/P-Masstree/
+    git pull
     rm -rf build && mkdir build
     cd build
     cmake .. && make -j
-    cd ..
+
     echo "" && echo "############" && echo "rebuilt" && echo "############" && echo ""
   fi
 done
 
+cd /home/xiaoxiang/RECIPE/P-Masstree/
 /home/blepers/linux/tools/perf/perf record ./build/example "$1" "$2"
