a6cbce375504640a49f8bf47806f995988b4980f
William <xiwu9085@uni.sydney.edu.au>
2022-04-23 15:12:19 +1000

    -AUTO- Sat Apr 23 15:12:19 AEST 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -105,12 +105,14 @@ void run(char **argv) {
         tbb::parallel_for(tbb::blocked_range<uint64_t>(0, n), [&](const tbb::blocked_range<uint64_t> &range) {
             auto t = tree->getThreadInfo();
             for (uint64_t i = range.begin(); i != range.end(); i++) {
-                uint64_t *ret = reinterpret_cast<uint64_t *> (tree->get(keys[i], t));
+                void* ptr = tree->get(keys[i], t);
+//                uint64_t *ret = reinterpret_cast<uint64_t *> (tree->get(keys[i], t));
+                uint64_t *ret = reinterpret_cast<uint64_t *> (ptr);
                 if (*ret != keys[i]) {
                     std::cout << "wrong value read: " << *ret << " expected:" << keys[i] << std::endl;
                     throw;
                 }
-//                which_free(ret);
+                which_free(ptr);
             }
         });
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
