3e6edb5239b553ec04fdde585609259ceeb333a9
William <xiwu9085@uni.sydney.edu.au>
2022-05-02 15:22:42 +1000

    -AUTO- Mon May  2 15:22:42 AEST 2022

--- P-Masstree/plog.cpp
+++ P-Masstree/plog.cpp
@@ -72,6 +72,7 @@ int log_new() {
     int success = 0;
     pthread_mutex_lock(&lm_lock);
     for (int i = 0; i < lm.num_entries; i++) {
+        printf("entry %d is %d\n",i,lm.entries[i][0]);
         if (lm.entries[i][0] == AVAILABLE) {
             lm.entries[i][0] = OCCUPIED;
             success = 1;
