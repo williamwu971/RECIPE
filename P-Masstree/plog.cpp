e59f56a5ce5401aa66603e89dc1670e581a76878
William <xiwu9085@uni.sydney.edu.au>
2022-05-05 17:50:50 +1000

    -AUTO- Thu May  5 17:50:50 AEST 2022

--- P-Masstree/plog.cpp
+++ P-Masstree/plog.cpp
@@ -313,6 +313,7 @@ void *log_garbage_collection(void *arg) {
             log_release(gq.indexes[i]);
 
             printf("used %lu in one go\n", thread_log->curr - thread_log->base);
+            printf("distance %lu\n",current_ptr-base_ptr);
             if (thread_log->curr > thread_log->base + (LOG_SIZE / GAR_QUEUE_LENGTH) * i) die("incorrect volume used");
 
         }
