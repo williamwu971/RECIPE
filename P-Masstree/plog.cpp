7cec97be1a6f5595025131d67b41c8a5882f561f
William <xiwu9085@uni.sydney.edu.au>
2022-05-18 17:54:52 +1000

    -AUTO- Wed May 18 17:54:52 AEST 2022

--- P-Masstree/plog.cpp
+++ P-Masstree/plog.cpp
@@ -590,10 +590,10 @@ int log_start_perf(const char *perf_fn) {
 
     sprintf(command,
             "sudo /home/blepers/linux/tools/perf/perf stat -a --per-socket -e"
-            "uncore_imc_1/event=0xe7,umask=0x0/,uncore_imc_1/event=0xe7,umask=0x0/,"
-            "uncore_imc_4/event=0xe7,umask=0x0/,uncore_imc_4/event=0xe7,umask=0x0/,"
-            "uncore_imc_7/event=0xe7,umask=0x0/,uncore_imc_7/event=0xe7,umask=0x0/,"
-            "uncore_imc_10/event=0xe7,umask=0x0/,uncore_imc_10/event=0xe7,umask=0x0/ > %s &",
+            "uncore_imc_1/event=0xe3,umask=0x0/,uncore_imc_1/event=0xe7,umask=0x0/,"
+            "uncore_imc_4/event=0xe3,umask=0x0/,uncore_imc_4/event=0xe7,umask=0x0/,"
+            "uncore_imc_7/event=0xe3,umask=0x0/,uncore_imc_7/event=0xe7,umask=0x0/,"
+            "uncore_imc_10/event=0xe3,umask=0x0/,uncore_imc_10/event=0xe7,umask=0x0/ > %s &",
             perf_fn
     );
 
