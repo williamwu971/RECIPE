b3ae7db13e739810f6d7e820a8c4d78fd7b18dc7
William <xiwu9085@uni.sydney.edu.au>
2022-05-19 15:15:44 +1000

    -AUTO- Thu May 19 15:15:44 AEST 2022

--- P-Masstree/plog.cpp
+++ P-Masstree/plog.cpp
@@ -206,8 +206,8 @@ void log_init(uint64_t num_logs) {
 
     omp_set_num_threads(16);
 #pragma omp parallel for schedule(dynamic,1)
-    for (uint64_t fs=0;fs<file_size;fs+=CACHE_LINE_SIZE){
-        pmem_memset_persist(big_map+fs, 0, CACHE_LINE_SIZE);
+    for (uint64_t fs=0;fs<file_size;fs+=(2*1024*1024ULL)){
+        pmem_memset_persist(big_map+fs, 0, (2*1024*1024ULL));
     }
 
 
