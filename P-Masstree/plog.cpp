58bd89f591e93ed59bcc39edfb16afdd4568b426
William <xiwu9085@uni.sydney.edu.au>
2022-05-30 16:14:22 +1000

    -AUTO- Mon May 30 16:14:22 AEST 2022

--- P-Masstree/plog.cpp
+++ P-Masstree/plog.cpp
@@ -626,10 +626,10 @@ int log_start_perf(const char *perf_fn) {
             getpid(), perf_fn);
     perf_stat = 0;
 
-    sprintf(command,
-            "sudo /home/blepers/linux/tools/perf/perf stat -d -d -d -p %d > %s 2>&1 &",
-            getpid(), perf_fn
-    );
+//    sprintf(command,
+//            "sudo /home/blepers/linux/tools/perf/perf stat -d -d -d -p %d > %s 2>&1 &",
+//            getpid(), perf_fn
+//    );
 
 //    sprintf(command,
 //            "/home/blepers/linux/tools/perf/perf record -p %d -o %s -g >> perf.out 2>&1 &",
