70a13d6644a891860c9822dfd55f86b5c222a6bf
William <xiwu9085@gmail.com>
2022-09-01 18:50:50 +1000

    -AUTO- Thu Sep  1 06:50:50 PM AEST 2022

--- P-Masstree/run.bash
+++ P-Masstree/run.bash
@@ -44,12 +44,12 @@ index_location=("dram")
 #value_location=("log")
 #value_location=("obj")
 #num_threads=(1 3 5 7 9 11 13 15)
-num_threads=(1)
+num_threads=(16)
 use_perf="yes"
 record_latency="yes"
 num_of_gc=(8)
 
-workload=5000000
+workload=64000000
 key_order="random"
 #key_order="seq"
 value_size=256 # the size of the value impact performance a lot
