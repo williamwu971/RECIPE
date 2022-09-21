9f9d9420215a37b9effc11a40653ea8a9498679a
William <xiwu9085@gmail.com>
2022-09-21 18:37:10 +1000

    -AUTO- Wed Sep 21 06:37:10 PM AEST 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -6,7 +6,7 @@
 //#include "tbb/tbb.h"
 #include "plog.cpp"
 
-#define PMEM_POOL_SIZE (8*1024*1024*1024ULL)
+#define PMEM_POOL_SIZE (4*1024*1024*1024ULL)
 #define FOOTER 0xdeadbeef
 
 // todo: make templates/cpp (modular) <- important
