c4ec0c565ec5695f9f12e00bd200bd00bc032f2d
William <xiwu9085@gmail.com>
2022-09-21 19:35:18 +1000

    -AUTO- Wed Sep 21 07:35:18 PM AEST 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -302,7 +302,7 @@ static inline void masstree_branched_insert(
     }
 }
 
-static inline void masstree_branched_update(
+static inline uint64_t masstree_branched_update(
         masstree::masstree *tree,
         MASS::ThreadInfo t,
         uint64_t u_key,
@@ -313,9 +313,14 @@ static inline void masstree_branched_update(
     if (use_obj) {
 
         struct masstree_obj *mo = NULL;
+
+        uint64_t aa,bb;
+
         TX_BEGIN(pop) {
 
+                        rdtscll(aa)
                         PMEMoid ht_oid = pmemobj_tx_alloc(value_size, TOID_TYPE_NUM(struct masstree_obj));
+                        rdtscll(bb)
                         pmemobj_tx_add_range(ht_oid, 0, value_size);
 
                         mo = (struct masstree_obj *) pmemobj_direct(ht_oid);
@@ -346,6 +351,7 @@ static inline void masstree_branched_update(
                         }
             TX_END
         }
+        return bb-aa;
 
     } else if (use_log) {
         char *raw = (char *) log_malloc(value_size);
@@ -416,6 +422,7 @@ static inline void masstree_branched_update(
 
 
     }
+    return 0;
 }
 
 static inline void masstree_branched_lookup(
@@ -701,11 +708,12 @@ void *section_update(void *arg) {
 
 //            rdtscll(a)
 
-            masstree_branched_update(tree, t, keys[i], keys[i], 1);
+            uint64_t tmp = masstree_branched_update(tree, t, keys[i], keys[i], 1);
 
             rdtscll(b)
 //            latencies[i] = b - a;
             latencies[i] = b;
+            latencies[i] = tmp;
         }
     } else {
 
