ed43e8fa037eb7949e768e8b3c218c8ef11e711f
William <xiwu9085@gmail.com>
2022-09-23 15:59:44 +1000

    -AUTO- Fri Sep 23 03:59:44 PM AEST 2022

--- P-Masstree/simple_graph.py
+++ P-Masstree/simple_graph.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--t", default=[], nargs='+', help="title")
 parser.add_argument("--x", default=['iteration'], nargs='+', help="x title")
 parser.add_argument("--y", default=['latency'], nargs='+', help="y title")
-parser.add_argument("--r", default=[], nargs='+', help="read results")
+parser.add_argument("--r", default=None, help="read results")
 parser.add_argument("--xlim", default=0, type=int, help="x range")
 parser.add_argument("--ylim", default=0, type=int, help="y range")
 parser.add_argument("--fn", default="out.png", type=str, help="y range")
@@ -25,41 +25,42 @@ plt.figure(1, figsize=(19, 12))
 #
 #         plt.plot(data_read, label=fn)
 
-for fn in args.r:
-    with open(fn, "r") as data_file:
-        data_read = list(map(lambda x: int(x), data_file.read().splitlines()))
-        final_data = []
 
-        last_time = data_read[0]
-        nb_requests = 0
-        max_latency = 0
+with open(args.r, "r") as data_file:
+    data_read = list(map(lambda x: int(x), data_file.read().splitlines()))
+    final_data = []
 
-        for time in data_read:
-            diff = time - last_time
-            nb_requests += 1
+    last_time = data_read[0]
+    nb_requests = 0
+    max_latency = 0
 
-            if diff > max_latency:
-                max_latency = diff
+    for time in data_read:
+        diff = time - last_time
+        nb_requests += 1
 
-            if diff > 2000000:
-                last_time = time
-                op_per_second = nb_requests / (diff / 2000000)
+        if diff > max_latency:
+            max_latency = diff
 
-                if args.ylim != 0 and op_per_second > args.ylim:
-                    raise Exception("adjust ylim to {}".format(op_per_second))
+        if diff > 2000000:
+            last_time = time
+            op_per_second = nb_requests / (diff / 2000000)
 
-                for i in range(0, diff - 2000000, 2000000):
-                    final_data.append(op_per_second)
+            if args.ylim != 0 and op_per_second > args.ylim:
+                raise Exception("adjust ylim to {}".format(op_per_second))
 
-                nb_requests = 0
+            for i in range(0, diff - 2000000, 2000000):
+                final_data.append(op_per_second)
 
-        # print("{} total cycle: {}".format(fn, data_read[-1] - data_read[0]))
+            nb_requests = 0
 
-        plt.plot(final_data, label=fn)
-        # print(len(final_data))
+    # print("{} total cycle: {}".format(fn, data_read[-1] - data_read[0]))
 
-        with open("max_latencies.txt", "a") as late_file:
-            print("{} max:{}".format(args.fn, max_latency), file=late_file)  # hack
+    # plt.plot(final_data, label=fn)
+    plt.plot(final_data)
+    # print(len(final_data))
+
+    with open("max_latencies.txt", "a") as late_file:
+        print("{} max:{}".format(args.fn, max_latency), file=late_file)  # hack
 
 # temp_title = ' '.join(args.t)
 # plt.title(temp_title, fontsize=40)
