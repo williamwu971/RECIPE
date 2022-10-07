b0ff9ee7636feca07691fb442c7b744e50e26c56
William <xiwu9085@gmail.com>
2022-10-07 17:12:53 +1100

    -AUTO- Fri Oct  7 05:12:53 PM AEDT 2022

---
+++ P-Masstree/perf_stat_parser.py
@@ -0,0 +1,11 @@
+for j in range(0, 241, 8):
+    print(j + 16, end="\t")
+    for i in range(2):
+        fn = "stats/dram-ralloc-24-8-NF{}-{}b-0B-N-insert.perf.stat".format(i, j)
+        # print(fn)
+        with open(fn, "r") as file:
+            lines = file.read().splitlines()
+            for line in lines:
+                if "resource_stalls" in line:
+                    print(line, end=" ")
+    print()
