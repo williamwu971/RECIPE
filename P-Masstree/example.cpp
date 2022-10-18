127a4a2060c3b2f496e91473a672de0194d5dba4
William <xiwu9085@gmail.com>
2022-10-18 23:07:55 +1100

    -AUTO- Tue Oct 18 11:07:55 PM AEDT 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -1205,15 +1205,15 @@ int main(int argc, char **argv) {
             // read in all the pointers
             uint64_t *all_values = (uint64_t*) malloc(sizeof(uint64_t) * n * 2);
             auto info = tree->getThreadInfo();
-            uint64_t min =0;
-            assert(tree->scan(min, n, all_values, info) == n);
+//            uint64_t min =0;
+//            assert(tree->scan(min, n, all_values, info) == n);
 
             for (uint64_t xx = 0;xx<n;xx++){
-//                all_values[xx] = (uint64_t)tree->get(xx,info);
+                all_values[xx] = (uint64_t)tree->get(xx,info);
                 printf("ptr %p\n",(void*)(all_values[xx]));
-                if ((void*)(all_values[xx])==NULL){
-                    break;
-                }
+//                if ((void*)(all_values[xx])==NULL){
+//                    break;
+//                }
             }
 
             int num_leaf = tree->scan_leaf((uint64_t) 0, n,  (all_values + n), info);
