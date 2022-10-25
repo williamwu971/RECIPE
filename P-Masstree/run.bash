bfa5c5fd00bccf4e69b09bf2eb7c630e428f9eaf
William <xiwu9085@gmail.com>
2022-10-25 21:11:31 +1100

    -AUTO- Tue Oct 25 09:11:31 PM AEDT 2022

--- P-Masstree/run.bash
+++ P-Masstree/run.bash
@@ -71,7 +71,7 @@ record_latency="yes"
 
 #workload=16000000
 #workload=140000000
-workload=400000000 # todo: do not change this
+workload=430000000 # todo: do not change this
 #workload=1900
 #workload=100000000
 
