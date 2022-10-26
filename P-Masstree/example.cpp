97bca4dc1c2bfaf6b2fadcde20c8a09f16fdf665
William <xiwu9085@gmail.com>
2022-10-26 19:30:28 +1100

    -AUTO- Wed Oct 26 07:30:28 PM AEDT 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -1406,7 +1406,7 @@ int main(int argc, char **argv) {
          * section INSERT
          */
         run("insert", throughput_file, attrs, section_args, latencies, section_insert);
-        throw;
+//        throw;
 //        if (use_log) log_debug_print(1, show_log_usage);
     }
 
