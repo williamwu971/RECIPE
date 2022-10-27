b67a19be227fb626b6cb8a0947dcfd38c1b78ac2
William <xiwu9085@gmail.com>
2022-10-27 19:04:15 +1100

    -AUTO- Thu Oct 27 07:04:15 PM AEDT 2022

--- P-Masstree/run.bash
+++ P-Masstree/run.bash
@@ -94,6 +94,7 @@ extra_sizes=(256)
 #total_sizes=($(seq 40 8 256))
 #total_sizes=($(seq 40 24 256))
 total_sizes=(1024)
+total_sizes=($(seq 40 24 1024))
 
 #index_location=("dram" "ralloc" "obj")
 index_location=("dram")
