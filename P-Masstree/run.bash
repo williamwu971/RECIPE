e29db67b2ee0e3d8eb97c97c629fcfdd66fd5bbf
William <xiwu9085@gmail.com>
2022-10-28 20:27:04 +1100

    -AUTO- Fri Oct 28 08:27:04 PM AEDT 2022

--- P-Masstree/run.bash
+++ P-Masstree/run.bash
@@ -104,7 +104,7 @@ index_location=("dram")
 #value_location=("ralloc")
 #value_location=("log")
 #value_location=("obj")
-value_location=("log" "log-best" "ralloc")
+value_location=("ralloc" "log" "log-best")
 
 #num_threads=(1 3 5 7 9 11 13 15)
 #num_threads=(24)
