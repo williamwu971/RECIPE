03216093fda03a7ae8965114a6f5836820a75c29
William <xiwu9085@gmail.com>
2022-11-11 17:21:01 +1100

    -AUTO- Fri Nov 11 05:21:01 PM AEDT 2022

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -731,7 +731,7 @@ void ralloc_reachability_scan(masstree::masstree *tree) {
     // push pointers to ralloc's list single threaded
     starttime = std::chrono::system_clock::now();
 
-    for (int i = 0; i < num_thread; i++) {
+    for (int i = 0; i < LEAF_WIDTH; i++) {
         struct ralloc_ptr_list *curr = ptr_lists[i];
 
         while (curr != NULL) {
