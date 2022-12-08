d0fc5115d5c9e7917425ce357cba126aef1d9198
William <xiwu9085@gmail.com>
2022-12-08 16:33:42 +1100

    -AUTO- Thu Dec  8 04:33:42 PM AEDT 2022

--- P-Masstree/run.bash
+++ P-Masstree/run.bash
@@ -67,8 +67,8 @@ done
 cd build || exit
 #rm -f ./*.rdtsc ./*.png ./*.csv ./max_latencies.txt
 
-workload=430000000
-#workload=43000000
+#workload=430000000
+workload=43000000
 
 extra_sizes=(0)
 #extra_sizes=(256)
