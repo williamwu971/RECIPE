8dafeb01405ce693d1a82207264710d35457f53e
William <william.wu.xiaoxiang@gmail.com>
2023-05-24 14:21:32 +0200

    -AUTO- date

--- P-Masstree/example.cpp
+++ P-Masstree/example.cpp
@@ -1865,7 +1865,7 @@ int main(int argc, char **argv) {
         /**
          * section UPDATE
          */
-        masstree_shuffle(keys, num_key);
+//        masstree_shuffle(keys, num_key);
         run("update", throughput_file, section_args, latencies, section_update, interfere);
     }
 
